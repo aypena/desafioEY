41e8d68ef829917d0f74faebd6e3516184f40808
yorxander <yorxander@gmail.com>
2025-07-25 17:33:16 -0400

    Primer commit para creacion de proyecto desafioEY

---
+++ src/main/java/com/desafioey/apiusuarios/DesafioeyApplication.java
@@ -0,0 +1,13 @@
+package com.desafioey.apiusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioeyApplication.class, args);
+	}
+
+}
